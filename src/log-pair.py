1006efa569e9f23ae0795ee97f431645a31e64a4
benstringer <ben.stringer@gmail.com>
2018-04-03 22:21:44 +1000

    Open files for reading

--- src/log-pair.py
+++ src/log-pair.py
@@ -5,11 +5,24 @@
 
 import sys
 
+#debug = 0
+debug = 1
+
 # Check for args
 
 if len(sys.argv) < 3:
    sys.exit('Usage: %s file1 file2' % sys.argv[0])
 
-print "File1 : ", sys.argv[1]
-print "File2 : ", sys.argv[2]
+if debug:
+   print "File1 : ", sys.argv[1]
+   print "File2 : ", sys.argv[2]
+
+# Open the input files
+
+file1 = open(sys.argv[1], 'r')
+file2 = open(sys.argv[2], 'r')
+
+# Read input into dict
+
+# 
 
